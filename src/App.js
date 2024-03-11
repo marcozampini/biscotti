5f340974009ab36524610e4a0089b94cd28ecc98
Marco Zampini <marco@bloom-it.io>
2024-03-11 23:23:55 +0100

    Recipes and Ingredients data in a separate JSON file

--- src/App.js
+++ src/App.js
@@ -3,44 +3,24 @@
 import './App.css'
 import Ingredient from './components/Ingredient'
 
+import recipes from './recipes.json'
+
 function App() {
   return (
     <div className="App">
-      <h1>Biscotti cioccolato e mirtilli</h1>
+      <h1>{recipes[0].description}</h1>
       <table>
-        <Ingredient description="Uova" quantity="60" />
-        <tr>
-          <td>Cioccolato</td>
-          <td>139 g</td>
-        </tr>
-        <tr>
-          <td>Cacao</td>
-          <td>28 g</td>
-        </tr>
-        <tr>
-          <td>Eritritolo</td>
-          <td>28 g</td>
-        </tr>
-        <tr>
-          <td>Mirtilli</td>
-          <td>167 g</td>
-        </tr>
-        <tr>
-          <td>Burro</td>
-          <td>139 g</td>
-        </tr>
-        <tr>
-          <td>Farina</td>
-          <td>390 g</td>
-        </tr>
-        <tr>
-          <td>Lievito</td>
-          <td>q. b.</td>
-        </tr>
-        <tr>
-          <td>Sale</td>
-          <td>q. b.</td>
-        </tr>
+        <tbody>
+          {recipes[0].ingredients.map((ingredient) => {
+            return (
+              <Ingredient
+                key={ingredient._id}
+                description={ingredient.description}
+                quantity={ingredient.quantity}
+              />
+            )
+          })}
+        </tbody>
       </table>
     </div>
   )
--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -1,8 +1,15 @@
+import React, { useState } from 'react'
+
 function Ingredient(props) {
+  const [quantity, setQuantity] = useState(props.quantity)
   return (
     <tr>
       <td>{props.description}</td>
-      <td>{props.quantity} g</td>
+      <td>
+        {quantity} g
+        <button onClick={() => setQuantity(quantity - 1)}> - </button>
+        <button onClick={() => setQuantity(quantity + 1)}> + </button>
+      </td>
     </tr>
   )
 }
