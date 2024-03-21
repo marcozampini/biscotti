c2c181988d770831532d1ff4721ff58705429e63
Marco Zampini <marco@bloom-it.io>
2024-03-21 01:03:10 +0100

    Add route for home page

--- src/App.js
+++ src/App.js
@@ -1,69 +1,17 @@
 // App.js
 
 import './App.css'
-import React, { useState } from 'react'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
-import Ingredient from './components/Ingredient'
-import IngredientWithoutWeight from './components/IngredientWithoutWeight'
-
-import recipes from './recipes.json'
+import Home from './pages/Home'
 
 function App() {
-  const initialQuantities = recipes[0].ingredients.map(
-    (ingredient) => ingredient.quantity
-  )
-  const [ingredientQuantities, setIngredientQuantities] =
-    useState(initialQuantities)
-
-  const handleIngredientChange = (index, quantity) => {
-    const newQuantities = [...ingredientQuantities]
-    const newRatio = quantity / initialQuantities[index]
-
-    newQuantities[index] = quantity
-
-    for (let i = 0; i < newQuantities.length; i++) {
-      if (i !== index) {
-        newQuantities[i] = Number(initialQuantities[i] * newRatio).toFixed(0)
-      }
-    }
-    setIngredientQuantities(newQuantities)
-  }
-
-  const handleReset = (e) => {
-    e.preventDefault()
-    setIngredientQuantities(initialQuantities)
-  }
-
   return (
     <div className="App">
-      <h1>{recipes[0].description}</h1>
-      <form className="recipe">
-        {recipes[0].ingredients.map((ingredient, index) => {
-          return (
-            <Ingredient
-              key={ingredient._id}
-              name={ingredient.name}
-              description={ingredient.description}
-              quantity={ingredientQuantities[index]}
-              onChange={(newQuantity) =>
-                handleIngredientChange(index, newQuantity)
-              }
-            />
-          )
-        })}
-        {recipes[0].ingredientsWithoutWeight.map((ingredient, index) => {
-          return (
-            <IngredientWithoutWeight
-              key={ingredient._id}
-              description={ingredient.description}
-              quantity={ingredient.quantity}
-            />
-          )
-        })}
-        <button type="reset" onClick={handleReset}>
-          Reset
-        </button>
-      </form>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,67 @@
+// Home.js
+
+import React, { useState } from 'react'
+import Ingredient from '../components/Ingredient'
+import IngredientWithoutWeight from '../components/IngredientWithoutWeight'
+
+import recipes from '../recipes.json'
+function Home() {
+  const initialQuantities = recipes[0].ingredients.map(
+    (ingredient) => ingredient.quantity
+  )
+  const [ingredientQuantities, setIngredientQuantities] =
+    useState(initialQuantities)
+
+  const handleIngredientChange = (index, quantity) => {
+    const newQuantities = [...ingredientQuantities]
+    const newRatio = quantity / initialQuantities[index]
+
+    newQuantities[index] = quantity
+
+    for (let i = 0; i < newQuantities.length; i++) {
+      if (i !== index) {
+        newQuantities[i] = Number(initialQuantities[i] * newRatio).toFixed(0)
+      }
+    }
+    setIngredientQuantities(newQuantities)
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    setIngredientQuantities(initialQuantities)
+  }
+  return (
+    <div>
+      <h1>{recipes[0].description}</h1>
+      <form className="recipe">
+        {recipes[0].ingredients.map((ingredient, index) => {
+          return (
+            <Ingredient
+              key={ingredient._id}
+              name={ingredient.name}
+              description={ingredient.description}
+              quantity={ingredientQuantities[index]}
+              onChange={(newQuantity) =>
+                handleIngredientChange(index, newQuantity)
+              }
+            />
+          )
+        })}
+        {recipes[0].ingredientsWithoutWeight.map((ingredient, index) => {
+          return (
+            <IngredientWithoutWeight
+              key={ingredient._id}
+              description={ingredient.description}
+              quantity={ingredient.quantity}
+            />
+          )
+        })}
+        <button type="reset" onClick={handleReset}>
+          Reset
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Home
